f49e3ff46a26427ae565fc325777858e5b38b211
Alen Babu <alenbabualen@gmail.com>
2023-11-10 19:05:55 +0530

    Header right content modified

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,11 +4,20 @@ import './header.css'
 function Header() {
     return (
         <div className='header'>
-            <div className='header-title-content'>
-            <img className='logo' src='./assets/zoho_logo.png ' alt='logo' />
-            <h1 className='header-title'>Campaigns</h1>
+            <div className='header-left-content'>
+                <img className='logo' src='./assets/zoho_logo.png ' alt='logo' />
+                <h1 className='header-title'>Campaigns</h1>
             </div>
-            
+
+            <div className='header-right-content'>
+                <h2>Trial expires in 10days</h2>
+                <button>Upgrade</button>
+                <i class="fa-solid fa-square-plus icon"></i>
+                <i class="fa-duotone fa-magnifying-glass"></i>
+
+            </div>
+
+
         </div>
     )
 }
