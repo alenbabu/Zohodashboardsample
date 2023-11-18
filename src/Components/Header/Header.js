3e2b64705ea7dd4a9cdfb6df5ed601c24eead2af
Alen Babu <alenbabualen@gmail.com>
2023-11-18 12:00:36 +0530

    header component is upgraded to tailwind version

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './header.css'
+
 import logo from "../../assets/zoho_logo.png"
 import avatar from "../../assets/avatar.png"
 
@@ -7,22 +7,22 @@ import avatar from "../../assets/avatar.png"
 
 function Header() {
     return (
-        <div className='header'>
-            <div className='header-left-content'>
-                <img className='logo' src={logo} alt='logo' />
-                <h1 className='header-title'>Campaigns</h1>
+        <div className='sticky w-full h-[7.5vh] z-[2] flex justify-between items-center bg-[white] border-b-[rgb(238,223,223)] border-b border-solid top-0'>
+            <div className='flex'>
+                <img className='w-[2em] h-[2em] cursor-pointer ml-[0.5em]' src={logo} alt='logo' />
+                <h1 className='text-[1.5rem] cursor-pointer ml-[0.2em] mt-[0.3em]'>Campaigns</h1>
             </div>
 
-            <div className='header-right-content'>
-                <h3 className='header-trial-title'>Trial expires in 10 days</h3>
-                <button className='header-upgrade-button'>Upgrade</button>
-                <i class="fa-solid fa-square-plus plus-icon icon"></i>
-                <i class="fa-solid fa-magnifying-glass icon"></i>
-                <i class="fa-regular fa-bell icon"></i>
-                <i class="fa-regular fa-circle-question icon"></i>
-                <i class="fa-solid fa-gear icon"></i>
-                <i class="fa-solid fa-ellipsis-vertical icon"></i>
-                <img className='header-avatar icon' src={avatar} alt='avatar' />
+            <div className='h-[7.5vh] flex justify-end items-center hover:bg-[#f8f6f6] hover:rounded'>
+                <h3 className='text-[#ff6a1a] font-[lighter] mr-[1.5em]'>Trial expires in 10 days</h3>
+                <button className='bg-[#FFEFED] text-[#FF3F00] rounded cursor-pointer mr-[1.2em] p-[0.4em] border-[none]'>Upgrade</button>
+                <i class="fa-solid fa-square-plus text-[#ff6a1a] text-[1.4em] cursor-pointer mr-[1.2em]"></i>
+                <i class="fa-solid fa-magnifying-glass cursor-pointer mr-[1.2em]"></i>
+                <i class="fa-regular fa-bell cursor-pointer mr-[1.2em]"></i>
+                <i class="fa-regular fa-circle-question cursor-pointer mr-[1.2em]"></i>
+                <i class="fa-solid fa-gear cursor-pointer mr-[1.2em]"></i>
+                <i class="fa-solid fa-ellipsis-vertical cursor-pointer mr-[1.2em]"></i>
+                <img className='rounded-[20px] cursor-pointer mr-[1.2em]' src={avatar} alt='avatar' />
 
             </div>
 
