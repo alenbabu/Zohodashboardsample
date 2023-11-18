5fb5014bbf3254860e14788f887fc639a9a9e546
Alen Babu <alenbabualen@gmail.com>
2023-11-18 11:49:56 +0530

    Sidenavbar is updated with tailwind css

--- src/Components/SideNavbar/Sidenavbar.js
+++ src/Components/SideNavbar/Sidenavbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './sidenavbar.css'
 import { AiOutlineDashboard, AiOutlineContacts } from "react-icons/ai"
 import { MdOutlineCampaign, MdLocalLibrary, MdOutlineReport } from "react-icons/md"
 import { TbSettingsAutomation } from "react-icons/tb"
@@ -20,23 +19,23 @@ function SideNavbar() {
     }
 
     return (
-        <div className='sidenavbar'>
-            <Link to="/dashboard" className='sidenavbar-content'>
+        <div className='bg-[#172635] w-[90px] h-screen flex flex-col items-center fixed text-[white]'>
+            <Link to="/dashboard" className='flex flex-col items-center  w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <AiOutlineDashboard style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Dashboard</h5>
+                <h5 className='font-[lighter] text-xs'>Dashboard</h5>
             </Link>
-            <Link to="/campaigns" className='sidenavbar-content'>
+            <Link to="/campaigns" className='flex flex-col items-center w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <MdOutlineCampaign style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Campaigns</h5>
+                <h5 className='font-[lighter] text-xs'>Campaigns</h5>
 
             </Link>
-            <div onClick={contactToggleDropdown} onMouseLeave={contactMouseLeave} className='sidenavbar-content'>
+            <div onClick={contactToggleDropdown} onMouseLeave={contactMouseLeave} className='flex flex-col items-center w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <AiOutlineContacts style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Contacts</h5>
+                <h5 className='font-[lighter] text-xs'>Contacts</h5>
                 {isDropdownopen && (
                     <div className="contacts-dropdown-menu">
                         <ul>
-                            <NavLink to="/contacts/allcontacts" className="active"><li>All Contacts</li></NavLink>
+                            <NavLink to="/contacts/allcontacts" className="no-underline text-[white]"><li>All Contacts</li></NavLink>
                             <li>Manage Lists</li>
                             <li>Manage Topics</li>
                             <li>Sync Service</li>
@@ -48,23 +47,23 @@ function SideNavbar() {
 
             </div>
 
-            <div className='sidenavbar-content'>
+            <div className='flex flex-col items-center w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <TbSettingsAutomation style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Automation</h5>
+                <h5 className='font-[lighter] text-xs'>Automation</h5>
             </div>
-            <div className='sidenavbar-content'>
+            <div className='flex flex-col items-center w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <BsCartCheck style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Ecommerce</h5>
+                <h5 className='font-[lighter] text-xs'>Ecommerce</h5>
 
             </div>
-            <div className='sidenavbar-content'>
+            <div className='flex flex-col items-center w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <MdLocalLibrary style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Library</h5>
+                <h5 className='font-[lighter] text-xs'>Library</h5>
 
             </div>
-            <div className='sidenavbar-content'>
+            <div className='flex flex-col items-center w-full no-underline text-[white] pt-[1.5em] hover:bg-[black] hover:border-b-2 hover:border-b-[cyan] hover:border-solid'>
                 <MdOutlineReport style={{ color: "white", fontSize: "1.5em" }} />
-                <h5 className='sidenavbar-titles'>Reports</h5>
+                <h5 className='font-[lighter] text-xs'>Reports</h5>
 
             </div>
 
